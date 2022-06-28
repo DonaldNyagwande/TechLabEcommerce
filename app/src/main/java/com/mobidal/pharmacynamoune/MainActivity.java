ced24422d564a871e0f3c048f5eda0a684e3d697
Youcef Ben Cheikh <bencheikh.youcef.se@gmail.com>
2022-06-29 00:09:25 +0100

    add toolbar to MainActivity

--- app/src/main/java/com/mobidal/pharmacynamoune/MainActivity.java
+++ app/src/main/java/com/mobidal/pharmacynamoune/MainActivity.java
@@ -1,15 +1,59 @@
 package com.mobidal.pharmacynamoune;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tb_main)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind Views
+        ButterKnife.bind(this);
+
+        // setup the {@link Toolbar}
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+            case R.id.action_shopping_basket:
+                shoppingBasketAction();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shoppingBasketAction() {
+
     }
 
 }
